7406555a4f51e4f84d0e76e49ee1f31f3d260389
senoa95 <sena@vt.edu>
2017-12-02 22:53:26 -0500

    this has been verified

---
+++ struct_tutorials.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+
+struct gradstudent{
+    string Name;
+    int PID;
+    int GPA;
+    string ResearchTopic;
+    string AdvisorName;
+    bool is_alive;
+  }sena,savio;
+
+void print (gradstudent student);
+
+int main()
+ {
+
+  sena.Name = "Sena Hunde";
+  sena.PID = 12345;
+  sena.GPA = 4;
+  sena.ResearchTopic = "agBOT";
+  sena.AdvisorName = "Leonessa";
+
+  savio.Name = "Savio Pereira";
+  savio.PID = 45623;
+  savio.GPA = 4;
+  savio.ResearchTopic = "Road Profiling";
+  savio.AdvisorName = "Ferris5";
+
+  cout<<" Info about Student 1"<<endl;
+
+  cout<<sena.Name<<", " <<sena.PID<<endl;
+
+    cout<<" Info about Student 2"<<endl;
+
+  cout<<savio.Name<<", " <<savio.PID<<endl;
+
+
+  return 0;
+}
+
+
+void print (gradstudent student)
+{
+  cout<<sena.Name<<endl;
+  cout<<savio.Name<<endl;
+}
