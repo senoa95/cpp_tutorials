bffea81edb527b045f6a13d547be4fe061679dc9
senoa95 <sena@vt.edu>
2017-12-04 21:48:41 -0500

    this function tutorial has been completed and tested

---
+++ cpp_function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+double multiply(double num1,double num2); //function declaration with arguments/inputs
+
+int main(int argc, char const *argv[]) {
+  double num1;  // Declaration of variable
+  double num2;
+  double product;
+
+  num1 = 65.4;  // Definition/initialization of the variable;
+  num2 = 84.2;
+
+  product = multiply(num1, num2);
+
+  cout<<product<<endl;
+
+  return 0;
+}
+
+double multiply(double num1,double num2){ //define the function here similar to defining variable in line 11
+
+  double product;
+  product = num1 * num2;
+
+  return product;
+}
